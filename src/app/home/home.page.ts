8930c658de391819e5712472941f0bd06d6ff0ca
Alice Aliaga <alicealiagamoreno@gmail.com>
2025-03-23 13:07:17 +0100

    Updated to S02 excercises

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,20 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
 
-  toDoList = ['Item 1', 'Item 2', 'Item 3'];
-  newItem = ''
+
+export class HomePage {
+  toDoList = [
+    { title: 'Task 1', edit: false },
+    { title: 'Task 2', edit: false },
+    { title: 'Task 3', edit: false }
+  ]
+
+  newTask = {
+    title: '',
+    edit: true
+  }
+
+  error = '';
 
   constructor() {}
 
   onNewItem() {
-    this.toDoList.push(this.newItem);
-    this.newItem = '';
+    if (this.canAddNewItem()) {
+      if (this.newTask.title != '') {
+        this.newTask.edit = false;
+        this.toDoList.push(this.newTask)
+        this.newTask = {title: '', edit: true};
+        this.error = '';
+      }
+      else {
+        this.error = 'La tarea neesita un nombre';
+      }
+    }
+    else {
+      this.error = 'Esta tarea ya existe';
+    }
   }
 
   onCompleteItem(i: any) {
     this.toDoList.splice(i, 1);
   }
 
+  canAddNewItem() {
+    for (let item of this.toDoList) {
+      if (item.title === this.newTask.title) return false;
+    }
+    return true;
+  }
 }
